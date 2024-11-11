6abb36504f917c7a6e68bd98bba1350a0d4a8dcd
Landon-5 <la.al.howard@gmail.com>
2024-11-11 13:17:05 -0700

    Made the project, fixed squigs, renamed content view to dropGameview.

---
+++ DropGame/DropGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropGameApp.swift
+//  DropGame
+//
+//  Created by Howard, Landon on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DropGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DropGameView()
+        }
+    }
+}
---
+++ DropGame/DropGameView.swift
@@ -0,0 +1,28 @@
+//
+//  DropGameView.swift
+//  DropGame
+//
+//  Created by Howard, Landon on 11/11/24.
+//
+
+import SwiftUI
+
+struct DropGameView: View 
+{
+    var body: some View 
+    {
+        VStack 
+        {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview ()
+{
+    DropGameView()
+}
