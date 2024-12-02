43dcc7bddb38639c9a28aac3d7b904ff19d3f70b
Landon-5 <la.al.howard@gmail.com>
2024-12-02 11:42:54 -0700

    made documents view and fixed squigs

---
+++ DropGame/ViewController/DocumentsView.swift
@@ -0,0 +1,21 @@
+//
+//  DocumentsView.swift
+//  DropGame
+//
+//  Created by Howard, Landon on 12/2/24.
+//
+
+import SwiftUI
+
+struct DocumentsView: View 
+{
+    var body: some View 
+    {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview 
+{
+    DocumentsView()
+}
