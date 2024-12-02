c72be185d62a17786803132f90e1c401d8c2f1b7
Landon-5 <la.al.howard@gmail.com>
2024-12-02 11:42:01 -0700

    Made MainView in ViewController and fixed squigs

---
+++ DropGame/ViewController/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  DropGame
+//
+//  Created by Howard, Landon on 12/2/24.
+//
+
+import SwiftUI
+
+struct MainView: View 
+{
+    var body: some View 
+    {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview 
+{
+    MainView()
+}
